2c39b0007afe2419e1b18d62290c20a0f79a2357
Jack Tisdell <jacktisdell@mail.com>
2017-08-05 20:04:52 -0400

    Write two-dimentional line drawing functions to establish proportions

--- hand.js
+++ hand.js
@@ -1,19 +1,61 @@
 function Phalanx(length, flexion, phalanx) {
   this.length = length;
-  this.flexion = flexion || null;
-  this.phalanx = phalanx || null;
+  this.flexion = flexion;
+  this.phalanx = phalanx;
+
+  this.draw = function() {
+    extend(this.length);
+    if (this.phalanx) {
+      this.phalanx.phalanx ? stroke(0, 0, 200) : stroke(200, 0, 0);
+      this.phalanx.draw();
+    }
+  }
 }
+// for distal phalanges, flexion and phalanx remain undefined
 
 function Metacarpal(length, flexion, abduction, phalanx) {
   this.length = length;
   this.flexion = flexion;
   this.abduction = abduction;
   this.phalanx = phalanx;
+
+  this.draw = function() {
+    stroke(255, 140, 0);
+    extend(this.length);
+    rotate(-this.abduction);
+    stroke(0, 200, 0);
+    this.phalanx.draw();
+  }
 }
 
 function CarpalGroup(thumbFlexion, thumbAdduction, metacarpalSpread, digits) {
-  this.thumbFlexion = thumbflexion;
+  this.thumbFlexion = thumbFlexion;
   this.thumbAdduction = thumbAdduction;
-  this.metacarpalSpreal = metacarpalSpread;
+  this.metacarpalSpread = metacarpalSpread;
   this.digits = digits; // array of Metacarpal objects with digits[0] for thumb
+
+  this.draw = function() {
+    var base = 0.6; //width of carpal group
+    var spread = map(this.metacarpalSpread, 0, 1, 1, 1.4);
+    line(-base/2, 0, base/2, 0);
+    translate(base/2, 0);
+    push();
+      rotate(-PI/6*spread + this.thumbAdduction); // abduct thumb metacarpal
+      this.digits[0].draw();
+    pop();
+    push();
+      for (var i = 0; i < 4; i++) {
+        translate(-base/4, 0);  // translate to metacarpal base
+        push();
+          rotate(PI/36*spread * (i-1)); // abduct metacarpal bone
+          this.digits[i+1].draw();
+        pop();
+      }
+    pop();
+  }
+}
+
+function extend(x) {
+  line(0, 0, 0, x);
+  translate(0, x);
 }
--- sketch.js
+++ sketch.js
@@ -1,41 +1,54 @@
-var thumb = new Metacarpal(1, PI/6, 0,
-              new Phalanx(1, 0,
-                new Phalanx(1)
+var thumb = new Metacarpal(0.5, 0, -0.1,
+              new Phalanx(0.4, 1,
+                new Phalanx(0.2)
               )
             );
-
-var index = new Metacarpal(2, 0, 0
-              new Phalanx(1, 0,
-                new Phalanx(1, 0,
-                  new Phalanx(1)
+var index = new Metacarpal(1, 0, 0,
+              new Phalanx(.55, 0,
+                new Phalanx(0.4, 0,
+                  new Phalanx(0.2)
                 )
               )
             );
-
-var middle = new Metacarpal(2, 0, 0
-              new Phalanx(1, 0,
-                new Phalanx(1, 0,
-                  new Phalanx(1)
+var middle = new Metacarpal(1, 0, 0,
+              new Phalanx(.6, 0,
+                new Phalanx(0.4, 0,
+                  new Phalanx(0.2)
                 )
               )
             );
-
-var ring = new Metacarpal(2, 0, 0
-              new Phalanx(1, 0,
-                new Phalanx(1, 0,
-                  new Phalanx(1)
+var ring = new Metacarpal(0.9, 0, 0.03,
+              new Phalanx(.6, 0,
+                new Phalanx(0.4, 0,
+                  new Phalanx(0.2)
                 )
               )
             );
-
-var pinky = new Metacarpal(2, 0, 0
-              new Phalanx(1, 0,
-                new Phalanx(1, 0,
-                  new Phalanx(1)
+var pinky = new Metacarpal(0.75, 0, 0.05,
+              new Phalanx(.55, 0,
+                new Phalanx(0.3, 0,
+                  new Phalanx(0.2)
                 )
               )
             );
-
 var digits = [thumb, index, middle, ring, pinky];
-
 var hand = new CarpalGroup(0, 0, 0, digits);
+
+
+function setup() {
+  createCanvas(600, 600);
+
+}
+
+function draw() {
+  background('#eee');
+  translate(width/2, height - 100);
+  scale(1, -1);
+  coordScale(150);
+  hand.draw();
+}
+
+function coordScale(r) {
+  scale(r);
+  strokeWeight(1/r);
+}
